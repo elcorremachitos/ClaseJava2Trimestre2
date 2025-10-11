3b08068d07718b39e46d77319892a288ab02f796
elcorremachitos <elcorremachitos.2015@gmail.com>
2025-10-10 21:06:56 -0500

    Initial commit

---
+++ src/Ejercicio1/Main.java
@@ -0,0 +1,14 @@
+package Ejercicio1;
+
+public class Main {
+    public static void main(String[] args) {
+        int list_Prices[] = {
+                1250, // Flor 1
+                1100, // Flor 2
+                1205, // Flor 3
+                1050, // Flor 4
+                1000, // Flor 5
+                1200 // Flor 6
+        };
+    }
+}
